bd86bcc9c7aecab73d57444cbc951747156ebfe5
Rodolfo Carvalho <rodolfo.ueg@gmail.com>
2009-08-13 10:46:16 -0300

    following google's tutorial

--- main.py
+++ main.py
@@ -6,7 +6,7 @@
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#     http://www.apache.org/licenses/LICENSE-2.0
+#         http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -15,26 +15,25 @@
 # limitations under the License.
 #
 
-
-
-
-import wsgiref.handlers
-
-
+# import wsgiref.handlers
 from google.appengine.ext import webapp
+from google.appengine.ext.webapp.util import run_wsgi_app
 
 
 class MainHandler(webapp.RequestHandler):
 
-  def get(self):
-    self.response.out.write('Hello world!')
+    def get(self):
+        self.response.headers['Content-type'] = 'text/plain'
+        self.response.out.write('Hello world! :D')
 
+
+application = webapp.WSGIApplication([('/', MainHandler)],
+                                     debug=True)
 
 def main():
-  application = webapp.WSGIApplication([('/', MainHandler)],
-                                       debug=True)
-  wsgiref.handlers.CGIHandler().run(application)
+    run_wsgi_app(application)
+    # wsgiref.handlers.CGIHandler().run(application)
 
 
 if __name__ == '__main__':
-  main()
+    main()
