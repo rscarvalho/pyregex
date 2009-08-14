f68192d15f05b7780955cfe775bea2bff8bd6862
Rodolfo Carvalho <rodolfo@rcarvalho.eti.br>
2009-08-14 08:23:43 -0300

    Added LICENSE, COPYING files, and License in file headers
    This software will be licensed by GNU Public License v2

--- main/forms.py
+++ main/forms.py
@@ -1,3 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# pyregex - A Google AppEngine application to test python regular expressions
+# Copyright (C) 2009  Rodolfo Carvalho
+# 
+# Author: Rodolfo Carvalho <rodolfo@rcarvalho.eti.br>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+
 from django import forms
 import re
 import sre_constants
@@ -9,8 +31,8 @@ class RegexForm(forms.Form):
     def clean_regex(self):
         try:
             # If regex is None, force compile to faile
-            re.compile(self.cleaned_data.get('regex', '(Invalid Regex'))
+            regex = re.compile(self.cleaned_data.get('regex', '(Invalid Regex'))
         except sre_constants.error, e:
             raise forms.ValidationError("Invalid Regexp")
         
-        return self.cleaned_data['regex']
+        return regex
--- main/views.py
+++ main/views.py
@@ -1,3 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# pyregex - A Google AppEngine application to test python regular expressions
+# Copyright (C) 2009  Rodolfo Carvalho
+# 
+# Author: Rodolfo Carvalho <rodolfo@rcarvalho.eti.br>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.http import Http404, HttpResponse
@@ -19,7 +41,7 @@ def check_regex(request):
     form = RegexForm(request.POST)
     
     if form.is_valid():
-        match = re.match(form.cleaned_data['regex'], form.cleaned_data['test_text'])
+        match = form.cleaned_data['regex'].match(form.cleaned_data['test_text'])
         return render_to_response("main/result.html", {'match': match})
     else:
         errors = form.errors
