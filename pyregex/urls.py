08fc5078d5045625ac968925f50027979cd0627e
Rodolfo Carvalho <rodolfo@infweb.net>
2013-07-24 06:59:08 -0300

    renamed main.py to pyregex/webapp.py for better packaging of the app

--- pyregex/urls.py
+++ pyregex/urls.py
@@ -2,12 +2,12 @@ import inspect
 import logging
 import webapp2
 
-def discover_resources(module, endpoint='/api'):
-	resources = [getattr(module, cls) for cls in dir(module)]
-	resources = [cls for cls in resources if inspect.isclass(cls) and issubclass(cls, webapp2.RequestHandler)]
-	resources = map(lambda r: [(url, r) for url in r.__urls__] if hasattr(r, '__urls__') else [], resources)
-	resources = reduce(lambda x, y: x + y, resources)
+def discover_resources(module, endpoint='/'):
+    resources = [getattr(module, cls) for cls in dir(module)]
+    resources = [cls for cls in resources if inspect.isclass(cls) and issubclass(cls, webapp2.RequestHandler)]
+    resources = map(lambda r: [(url, r) for url in r.__urls__] if hasattr(r, '__urls__') else [], resources)
+    resources = reduce(lambda x, y: x + y, resources)
 
-	logging.info('Registering URLs for endpoint "%s": %s' % (endpoint, resources))
+    logging.info('Registering URLs for endpoint "%s": %s' % (endpoint, resources))
 
-	return [("%s/%s" % (endpoint, url), handler) for url, handler in resources]
+    return [("%s/%s" % (endpoint, url), handler) for url, handler in resources]
