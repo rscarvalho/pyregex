8e8a43c96dd0736124746e9f51b999505e7e2f39
Rodolfo Carvalho <rodolfo@infweb.net>
2013-07-28 16:34:16 -0300

    added deploy phase to Travis

---
+++ deploy.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env sh
+
+deploy_branch='stable'
+current_branch=$(git branch | grep '*' | sed -e 's/^\* *//')
+
+# Only deploy from the stable branch
+if [[ "$current_branch" != "$deploy_branch" ]]; then
+    echo "You cannot deploy from branch $current_branch." >&2
+    exit 0
+fi
+
+# Set default path for the Google AppEngine SDK
+if [[ -z "$APPENGINE_SDK_PATH" ]]; then
+    APPENGINE_SDK_PATH='/usr/local/bin'
+fi
+
+echo "Using AppEgine SDK at \"${APPENGINE_SDK_PATH}\""
+
+if [[ -z "$APPENGINE_PASSWORD" || -z "$APPENGINE_EMAIL" ]]; then
+    echo "ERROR: Please set APPENGINE_EMAIL and APPENGINE_PASSWORD" >&2
+    exit -1
+fi
+
+echo $APPENGINE_PASSWORD | $APPENGINE_SDK_PATH/appcfg.py update . -e $APPENGINE_EMAIL --passin
+
+exit $?
