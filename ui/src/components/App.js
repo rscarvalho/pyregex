e62476187dbb32f80c89d914789e1bd252d50833
Rodolfo Carvalho <rodolfo@spotify.com>
2017-11-12 17:02:08 -0500

    Added the basic bootstrap skeleton for the app
    The color theme is still off, but it's a good start already.

--- ui/src/components/App.js
+++ ui/src/components/App.js
@@ -1,3 +1,18 @@
+import Col from 'react-bootstrap/lib/Col';
+import Grid from 'react-bootstrap/lib/Grid';
+import NavBar from './NavBar';
 import React from 'react';
+import Row from 'react-bootstrap/lib/Row';
 
-export default () => <noscript />;
+export default function App() {
+  return (
+    <div className="app">
+      <NavBar />
+      <Grid fluid>
+        <Row>
+          <Col size={12}>The rest of the app goes here</Col>
+        </Row>
+      </Grid>
+    </div>
+  );
+}
--- ui/src/index.js
+++ ui/src/index.js
@@ -1,3 +1,4 @@
+import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 
 import App from './components/App';
