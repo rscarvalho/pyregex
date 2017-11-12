e78eca612d4fe2bd6f658d3a41cce55664340254
Rodolfo Carvalho <rodolfo@spotify.com>
2017-11-12 16:01:14 -0500

    Added react-bootstrap as a dependency
    Also started moving a few source files from the legacy app

--- ui/src/index.js
+++ ui/src/index.js
@@ -1,6 +1,6 @@
 import './index.css';
 
-import App from './App';
+import App from './components/App';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
