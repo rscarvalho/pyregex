2ad14a017ca42e59a27b79bd05ccd619ebe6d4a3
Rodolfo Carvalho <rodolfo.ueg@gmail.com>
2020-04-19 21:30:10 -0400

    Removed a bunch of unnecessary files
    Also properly setup rollbar

--- pyregex/api.py
+++ pyregex/api.py
@@ -1,15 +1,18 @@
 import os
-from os.path import join, dirname, abspath, exists, realpath
-from flask import Flask, request, make_response
-from flask.json import jsonify
-from flask import got_request_exception
-from pyregex.middleware import CORSMiddleware
-from pyregex.service import RegexService, InvalidRegexError, UnprocessibleRegexError
+from base64 import b64decode
+from os.path import abspath, dirname, exists, join, realpath
+
 import rollbar
 import rollbar.contrib.flask
+from flask import Flask, got_request_exception, make_response, request
+from flask.json import jsonify
+from flask_cors import CORS
+
+from pyregex.middleware import CORSMiddleware
+from pyregex.service import (InvalidRegexError, RegexService,
+                             UnprocessibleRegexError)
 
 app = Flask('pyregex')
-app.secret_key = '\x0f0%T\xd3\xd5\x11\xca\xaa\xf5,\x02Zp,"\x83\x94\x1b\x9e|6\xd7<'
 
 
 @app.before_first_request
@@ -27,11 +30,32 @@ def init_rollbar():
         app.logger.info("Rollbar token not present. Skipping rollbar setup")
 
 
+if 'FLASK_SECRET_KEY' in os.environ:
+    app.secret_key = b64decode(os.environ['FLASK_SECRET_KEY'])
+    pass
+else:
+    app.secret_key = '\x0f0%T\xd3\xd5\x11\xca\xaa\xf5,\x02Zp,"\x83\x94\x1b\x9e|6\xd7<'
+
+CORS(app)
+
+
 def api_error(message, *args, **kwargs):
     response = jsonify(result_type='error', message=message % args)
     response.status_code = int(kwargs.get('status', 400))
     response.headers['Content-type'] = 'application/json'
     return response
+
+
+@app.before_first_request
+def init_rollbar():
+    """Initializes the rollbar module
+    """
+    token = os.environ.get("ROLLBAR_TOKEN")
+    environment = os.environ.get("ROLLBAR_ENV", "development")
+    if token:
+        rollbar.init(token, environment, root=dirname(realpath(__file__)))
+    else:
+        app.logger.info("Rollbar token not present. Skipping rollbar setup")
 
 
 @app.route('/api/regex/test/', methods=['GET'])
@@ -64,9 +88,6 @@ def test_regex():
 
     return jsonify(result_type=match_type, result=result)
 
-
-setup_logging(app)
-app = CORSMiddleware(app)
 
 if __name__ == '__main__':
     from werkzeug.serving import run_simple
